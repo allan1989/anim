426d0ff448d781289ac6e60fe3396b834808a531
Allan ROLLE <allan.rolle@laposte.net>
2021-10-16 16:16:32 +0200

    final version

--- test-project/src/app/app.component.ts
+++ test-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { gsap } from "gsap";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(){}
+  @ViewChild('form') form: ElementRef;
+  @ViewChild('result') result: ElementRef;
   title = 'test-project';
+
+  tl = gsap.timeline();
+
+  
+  showResults() {
+    if(!this.tl.isActive()){
+      this.tl
+      .fromTo(this.form.nativeElement,{x: 0, duration: 1}, {x: -125} )
+      .fromTo(this.result.nativeElement, {opacity: 0, x:125, duration: 1}, {opacity: 1, delay: 0.250})
+    }
+  }
+  
+  hideResults(){
+    if(parseInt(window.getComputedStyle(this.result.nativeElement,null).getPropertyValue("opacity")) === 0) return;
+    this.tl
+        .fromTo(this.result.nativeElement, {opacity: 1, duration: 2}, {opacity: 0, delay: 0.250})
+        .fromTo(this.form.nativeElement,{x: -125, duration: 1}, {x: 0, delay: 0.250} )
+  }
 }
